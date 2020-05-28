e0a3d95163c2b09f18a51eebc2c5cff5332db615
Samar <33544311+god-s-perfect-idiot@users.noreply.github.com>
2020-05-28 14:13:00 +0530

    Add files via upload

--- models.py
+++ models.py
@@ -40,5 +40,56 @@ class Schema:
         self.connection.commit()
 
 
-class Manage:
-    table = 
+class ManageUser:
+    def __init__(self):
+        try:
+            self.connection = mysql.connector.connect(host='localhost',database='tsf',user='root',password='root')
+            if(self.connection.is_connected()):
+                self.cursor = self.connection.cursor()
+                self.cursor.execute("select database();")
+                record = self.cursor.fetchone()
+
+        except Error as e:
+            print("Error connection to db",e)
+
+    def getuserlist(self):
+        query = "SELECT * FROM user;"
+        result = self.cursor.execute(query)
+        return result
+
+    def getuserdetails(self,id):
+        query = "SELECT * FROM user WHERE user_id="+id+";"
+        result = self.cursor.execute(query)
+        return result
+
+    def updatecreds(self,id1,id2,crd):
+        query = "UPDATE user SET credit=credit-"+crd+"where user_id="+id1+";"
+        result = self.cursor.execute(query)
+        self.connection.commit()
+
+        query = "UPDATE user SET credit=credit+"+crd+"where user_id="+id2+";"
+        result = self.cursor.execute(query)
+        self.connection.commit()
+
+
+class ManageTrs:
+    def __init__(self):
+        try:
+            self.connection = mysql.connector.connect(host='localhost',database='tsf',user='root',password='root')
+            if(self.connection.is_connected()):
+                self.cursor = self.connection.cursor()
+                self.cursor.execute("select database();")
+                record = self.cursor.fetchone()
+
+        except Error as e:
+            print("Error connection to db",e)
+
+    def addtrn(self,id1,id2,crd):
+        query = "INSERT INTO transfers(sender_id,recipient_id,credits) VALUES("+id1+","+id2+","+crd");"
+        result = self.cursor.execute(query)
+        self.connection.commit()
+
+    def gettrn(self,id):
+        query = "SELECT * FROM transfers WHERE sender_id="+id+";"
+        result = self.cursor.execute(query)
+        self.connection.commit()
